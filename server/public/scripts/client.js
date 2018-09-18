eba3d9e4ecf3ec8f6e5950d016aa8e11d824a235
Christopher Brenberg <cbrenberg@gmail.com>
2018-09-18 16:07:01 -0500

    add green route

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -6,11 +6,18 @@ colorApp.config(['$routeProvider', function ($routeProvider) {
       template: '<h1>Home</h1>',
     })
     .when('/red', {
-      templateUrl: 'views/red.html'
+      templateUrl: 'views/red.html',
+      controller: 'RedController as vm'
     })
     .when('/blue', {
-      templateUrl: 'views/blue.html'
-    }).otherwise({
+      templateUrl: 'views/blue.html',
+      controller: 'BlueController as vm'
+    })
+    .when('/green', {
+      templateUrl: 'views/green.html',
+      controller: 'GreenController as vm'
+    })
+    .otherwise({
       template: '<h1>404</h1>'
     })
 }]);
@@ -31,3 +38,10 @@ colorApp.controller('BlueController', ['$http', function ($http) {
   vm.message = 'Blue page loaded!'
 }])//end BlueController
 
+colorApp.controller('GreenController', ['$http', function ($http) {
+  console.log('Running GreenController');
+
+  let vm = this;
+
+  vm.message = 'Green page loaded!'
+}])//end GreenController
